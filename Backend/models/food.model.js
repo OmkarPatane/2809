25f7d568183031026fe166e8a36eb00694910562
Vinita <vinitaveerani@gmail.com>
2025-01-18 20:33:59 +0530

    food API added

--- Backend/models/food.model.js
+++ Backend/models/food.model.js
@@ -6,7 +6,32 @@ dishname:{
     type:String,
     required:true,
 },
-ingredients:{
-    type:Array
-}
+items:[
+    {
+        name:{
+        type:String,
+        required:true
+        },
+        quantity:{
+            type:String,
+            default:"1 serving"
+        }
+    }
+    
+],
+QRcode:String
 })
+
+const dishes=model("dish",dishSchema)
+
+const calorieSchema=new Schema({
+    itemname:String,
+    quantity:{
+        type:String,
+        enum:["1 pc","1 serving"]
+    },
+    calorie:Number
+
+})
+const calories=model("calorie",calorieSchema)
+export {dishes,calories}
